0dd993a115412a16521affffd233b6c49663c2e7
PvtSec <akssofts@gmail.com>
2019-05-12 15:45:23 +0530

    drawable commit 1

--- app/src/main/java/com/digi/edu/gecw/LoginActivity.java
+++ app/src/main/java/com/digi/edu/gecw/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.digi.edu.gecw;
 
+
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,29 +10,51 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 
 
+
 public class LoginActivity extends AppCompatActivity
 {
     Button loginBtn;
     EditText username,password;
     ProgressBar spin;
+    Typeface fontstyle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        fontstyle =Typeface.createFromAsset(getAssets(),  "fonts/font.ttf");
         setContentView(R.layout.activity_login);
         loginBtn=findViewById(R.id.login_button);
         username=findViewById(R.id.username_entry);
         password=findViewById(R.id.password_entry);
         spin=findViewById(R.id.loading_login);
+        username.setTypeface(fontstyle);
 
         loginBtn.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                loginBtn.setVisibility(View.GONE);
-                spin.setVisibility(View.VISIBLE);
+                if(username.getText().toString().matches(""))
+                {
+                    username.setHintTextColor(getResources().getColor(R.color.Red));
+                    username.setHint("Invalid Username");
+                }
+                else
+                {
+                    username.setHint("Not null");
+                }
+                if(password.getText().toString().matches(""))
+                {
+                    password.setHintTextColor(getResources().getColor(R.color.Red));
+                    password.setHint("Invalid Password");
+                }
+                else
+                {
+                    password.setHint("Not null");
+                }
+                //loginBtn.setVisibility(View.GONE);
+                //spin.setVisibility(View.VISIBLE);
             }
         });
     }
