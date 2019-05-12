1abc3be59a428983bce85cd107558b14cbe00e25
PvtSec <akssofts@gmail.com>
2019-05-12 14:33:35 +0530

    first commit

--- app/src/main/java/com/digi/edu/gecw/LoginActivity.java
+++ app/src/main/java/com/digi/edu/gecw/LoginActivity.java
@@ -2,12 +2,22 @@ package com.digi.edu.gecw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+
+public class LoginActivity extends AppCompatActivity
+{
+    Button loginBtn;
+    EditText username,password;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginBtn=findViewById(R.id.login_button);
+        username=findViewById(R.id.username_entry);
+        password=findViewById(R.id.password_entry);
     }
 }
